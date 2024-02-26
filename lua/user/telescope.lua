6b006e95c70ae35494deebf212244c6979ea71de
witja46 <witja46@gmail.com>
2024-02-26 15:27:15 +0100

    Init

---
+++ lua/user/telescope.lua
@@ -0,0 +1,34 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+}
+M.config = function ()
+  local actions = require "telescope.actions"
+  local telescope = require("telescope")
+  telescope.setup({
+
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+    mappings = {
+      i = {
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
+  }
+  })
+  telescope.load_extension("fzf")
+end
+
+
+
+return M
